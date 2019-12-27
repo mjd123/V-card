8eefd998e458d529f9a8439a1a0ed8712c5ba8db
Marcus <mj88denny@googlemail.com>
2019-12-27 17:50:02 +0000

    removed website link icons

--- src/components/name/name.js
+++ src/components/name/name.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './name.css';
 import Linkedin from './img/linkedin-icon.png'
 import Github from './img/github-icon.png';
-import Desktop from './img/desktop.svg';
+//import Desktop from './img/desktop.svg';
 
 class myName extends Component {
     constructor(props) {
@@ -21,7 +21,7 @@ class myName extends Component {
             <div className="icons">
                 <a href="https://www.linkedin.com/in/marcus-y-518a7a11a/" target="_blank" rel="noopener noreferrer"><img src={Linkedin} alt="Linkedin link"></img></a>
                 <a href="https://github.com/mjd123" target="_blank" rel="noopener noreferrer"><img src={Github} alt="Github link"></img></a>
-                <a><img src={Desktop} alt="Website link"></img></a>
+                {/* <a><img src={Desktop} alt="Website link"></img></a> */}
             </div>
         </article>
       );
